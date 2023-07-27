f8b8f0649451e5b4d181284473458425ec8db8f1
Abner Macias <abnermacias@mtech-systems.com>
2023-07-27 13:08:46 -0600

    Add Camera + Files Button

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { Chooser } from '@awesome-cordova-plugins/chooser/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
+  providers: [Chooser],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Chooser } from '@awesome-cordova-plugins/chooser/ngx';
+import {
+  Camera,
+  CameraPluginPermissions,
+  GalleryImageOptions,
+} from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private readonly chooser: Chooser) {}
 
-  constructor() {}
+  async handleImagePicker() {
+    const cameraPermissionStatus = await Camera.checkPermissions();
+    if (['granted', 'limited'].includes(cameraPermissionStatus.photos)) {
+      this.openPickerImage();
+    } else {
+      const permissions = {} as CameraPluginPermissions;
+      permissions.permissions = ['photos'];
+      Camera.requestPermissions(permissions);
+    }
+  }
 
+  //OPEN IMAGE FROM GALLERY
+  openPickerImage() {
+    const options = {
+      quality: 100,
+    } as GalleryImageOptions;
+    Camera.pickImages(options);
+  }
+
+  //OPEN FILE FROM DEVICE
+  openChooser() {
+    this.chooser.getFile();
+  }
 }
